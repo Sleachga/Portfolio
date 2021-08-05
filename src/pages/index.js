26b0cf620532b8dafe1e6dc8f0d9e7915f5ca88c
Sleachga <leach.sandy3@gmail.com>
2021-08-05 18:55:23 -0400

    Added typewriter effect to the main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,56 @@
-import * as React from 'react'
-import Layout from '../components/layout'
-import { StaticImage } from 'gatsby-plugin-image'
+import * as React from "react";
+import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
+import styled from "styled-components";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowDown, faArrowUp } from "@fortawesome/free-solid-svg-icons";
+
+import Typewriter from "typewriter-effect";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const Text = styled.div`padding-right: 4px;`
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Hey there 👋">
-      <p>I'm Sanford.  Welcome to my blog/portfolio/personal space.</p>
-      <p>Thats me hiking <FontAwesomeIcon icon={faArrowDown} /></p>
+      <p>
+        <div>
+          <Container>
+            <Text>I'm Sanford. Welcome to my</Text>
+            <Typewriter
+              options={{
+                strings: [
+                  " personal site",
+                  " blog",
+                  " portfolio",
+                  " creative space",
+                ],
+                autoStart: true,
+                cursor: "",
+                loop: true,
+              }}
+            />
+          </Container>
+        </div>
+      </p>
+      <p>
+        Thats me hiking <FontAwesomeIcon icon={faArrowDown} />
+      </p>
+      <p>
+        Click on the links above <FontAwesomeIcon icon={faArrowUp} /> to learn more about me
+      </p>
       <StaticImage
         alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
         src="../images/profile.jpeg"
       />
     </Layout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
