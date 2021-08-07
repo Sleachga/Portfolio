09dd045c553418cc5ffb762b525276704b840cc3
Sleachga <leach.sandy3@gmail.com>
2021-08-06 23:03:54 -0400

    Finishing touches for tonight (removing animation bg)

--- src/pages/fish.js
+++ src/pages/fish.js
@@ -1,11 +1,19 @@
-import * as React from 'react'
-import Layout from '../components/layout'
+import * as React from "react";
+import Layout from "../components/layout";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWrench } from "@fortawesome/free-solid-svg-icons";
+
 const Challenges = () => {
   return (
-    <Layout page='fish' pageTitle='Feed the Fish' />
-  )
-}
+    <Layout page="fish" pageTitle="Feed the Fish">
+      <p>
+        In construction <FontAwesomeIcon icon={faWrench} />
+      </p>
+    </Layout>
+  );
+};
 
 // TODO: maybe put a box bottom left that explains this page (click)
 
-export default Challenges
+export default Challenges;
