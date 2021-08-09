334ecbd735288b787a34624f94e9acb5b823977f
Sleachga <leach.sandy3@gmail.com>
2021-08-08 21:08:45 -0400

    updated lilypad style

--- src/animations/classes/lilypad.js
+++ src/animations/classes/lilypad.js
@@ -2,26 +2,18 @@ export class Lilypad {
   constructor(width, height) {
     this.x = Math.round(Math.random() * width);
     this.y = Math.round(Math.random() * height);
-    this.dx = (Math.random() - 0.5) * 1;
-    this.dy = (Math.random() - 0.5) * 1;
+    this.dx = (Math.random() - 0.5) * 0.5;
+    this.dy = (Math.random() - 0.5) * 0.5;
     this.startAngle = Math.random() * Math.PI * 2;
     this.endAngle = this.startAngle + Math.PI;
     this.rotateSpeed = Math.random() > 0.5 ? 0.005 : -0.005;
     this.petalLength = 12 + Math.random() * 4;
     this.radius = 20;
-    this.petalColors = [
-      '#ff00fa',
-      '#8c00ff',
-      '#ce3b63',
-      '#e00ba7',
-      '#ff35e4',
-      '#7c1531',
-      '#7c1531',
-    ];
+    this.petalColors = ['#E0BCDD', '#E17081', '#FF9D00', '#E53730', '#A25CB7'];
     this.petalColor =
-            this.petalColors[
-              Math.round(Math.random() * (this.petalColors.length - 1))
-            ];
+      this.petalColors[
+        Math.round(Math.random() * (this.petalColors.length - 1))
+      ];
   }
 
   draw(context) {
@@ -36,6 +28,8 @@ export class Lilypad {
     );
     context.fillStyle = '#199615';
     context.fill();
+    context.strokeStyle = '#4D5B1F';
+    context.stroke();
 
     context.beginPath();
     context.arc(
@@ -46,8 +40,11 @@ export class Lilypad {
       this.endAngle + 1.7 * (Math.PI / 2),
       false
     );
-    context.fillstyle = 'white';
+
+    context.fillstyle = '#199615';
     context.fill();
+    context.strokeStyle = '#4D5B1F';
+    context.stroke();
 
     // lines on lilypads
     for (let i = 0; i < 5; i++) {
@@ -55,15 +52,12 @@ export class Lilypad {
       context.moveTo(this.x, this.y);
       context.lineTo(
         this.x +
-                    (this.radius - 2) *
-                        Math.cos(this.startAngle + (Math.PI / 2.18) * i),
+          (this.radius - 2) * Math.cos(this.startAngle + (Math.PI / 2.18) * i),
         this.y +
-                    (this.radius - 2) *
-                        Math.sin(this.startAngle + (Math.PI / 2.18) * i)
+          (this.radius - 2) * Math.sin(this.startAngle + (Math.PI / 2.18) * i)
       );
-      context.lineWidth = 2;
-      // context.strokeStyle = "#023a03";
-      context.strokeStyle = '#024219';
+      context.lineWidth = 1;
+      context.strokeStyle = '#4D5B1F';
       context.stroke();
     }
 
@@ -82,6 +76,8 @@ export class Lilypad {
       );
       context.fillStyle = this.petalColor;
       context.fill();
+      context.strokeStyle = '#4D5B1F';
+      context.stroke();
     }
 
     for (let i = 0; i < 4; i++) {
@@ -103,8 +99,10 @@ export class Lilypad {
     //the middle part of the flower
     context.beginPath();
     context.arc(this.x, this.y, this.radius / 4, 0, 2 * Math.PI, false);
-    context.fillStyle = '#fffa00';
+    context.fillStyle = '#F9DF4B';
     context.fill();
+    // context.strokeStyle = '#4D5B1F';
+    // context.stroke();
   }
 
   update(width, height) {
